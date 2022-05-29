956511ea798cd5dbc8477429f72d3b025f1caf89
“Polibio <polibio.lins@gmail.com>
2022-05-29 16:44:59 -0300

    Correcao: Cor de background dos botoes

--- App.js
+++ App.js
@@ -177,7 +177,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#41029c',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90,
